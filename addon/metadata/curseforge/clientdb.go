b125f2e04524f8ccfba70650ad34531627d11492
Sweetlie <sweetlie@wipeinc.io>
2018-06-17 16:04:01 +0200

    better parsing

--- addon/metadata/curseforge/clientdb.go
+++ addon/metadata/curseforge/clientdb.go
@@ -3,31 +3,45 @@ package curseforge
 import (
 	"encoding/json"
 	"io"
+	"strings"
+	"time"
 )
 
 // ClientDB database root
 type ClientDB struct {
-	Data      []Addon `json:"data"`
+	Addons    []Addon `json:"data"`
 	Timestamp int64   `json:"timestamp"`
 }
 
 // Addon Curseforge addon struct
 type Addon struct {
-	ID            int64        `json:"Id"`
-	Name          string       `json:"Name"`
-	URL           string       `json:"WebSiteIURL"`
-	Summary       string       `json:"Summary"`
-	DownloadCount int64        `json:"DownloadCount"`
-	LatestFiles   []LatestFile `json:"LatestFiles"`
+	ID            int64     `json:"Id"`
+	Name          string    `json:"Name"`
+	URL           string    `json:"WebSiteIURL"`
+	Summary       string    `json:"Summary"`
+	DownloadCount float64   `json:"DownloadCount"`
+	Releases      []Release `json:"LatestFiles"`
 }
 
-// LatestFile Curseforge last release struct
-type LatestFile struct {
+// Release Curseforge last release struct
+type Release struct {
 	ID          int64    `json:"Id"`
 	Filename    string   `json:"Filename"`
-	Date        string   `json:"Filedate"`
+	Date        dbDate   `json:"Filedate"`
 	DownloadURL string   `json:"DownloadURL"`
 	GameVersion []string `json:"GameVersion"`
+}
+
+type dbDate struct {
+	time.Time
+}
+
+const dateLayout = "2006-01-02T15:04:05"
+
+func (d *dbDate) UnmarshalJSON(b []byte) (err error) {
+	s := strings.Trim(string(b), "\"")
+	d.Time, err = time.Parse(dateLayout, s)
+	return err
 }
 
 // DecodeDB decode client database
--- addon/metadata/curseforge/clientdb_test.go
+++ addon/metadata/curseforge/clientdb_test.go
@@ -1,17 +1,37 @@
 package curseforge
 
 import (
+	"log"
 	"os"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
 
+func TestIntegrationDecode(t *testing.T) {
+	t.SkipNow()
+	fixture, err := os.Open("./fixtures/clientdb/complete.json")
+	if err != nil {
+		t.Fatalf("error loading fixture: %s", err)
+	}
+	db, err := DecodeDB(fixture)
+	if err != nil {
+		t.Fatalf("unexpected error: %s", err)
+	}
+	const minAddonCount = 5000
+	if len(db.Addons) < minAddonCount {
+		log.Fatalf("expected number of addon, got > %d, got: %d\n",
+			minAddonCount, len(db.Addons))
+	}
+}
+
 func TestDecode(t *testing.T) {
-	latest := LatestFile{
+	date, err := time.Parse(dateLayout, "2017-09-20T13:00:12.89")
+	latest := Release{
 		ID:          2482102,
 		Filename:    "7.3.1",
-		Date:        "2017-09-20T13:00:12.89",
+		Date:        dbDate{date},
 		DownloadURL: "https://files.forgecdn.net/files/2482/102/Gatherer-7.3.1.zip",
 		GameVersion: []string{"7.3.0"},
 	}
@@ -20,12 +40,12 @@ func TestDecode(t *testing.T) {
 		Name:          "Gatherer",
 		Summary:       "Helps track the closest plants, deposits and treasure locations on your minimap.",
 		DownloadCount: 15465595,
-		LatestFiles:   []LatestFile{latest},
+		Releases:      []Release{latest},
 	}
 
 	gathererDB := &ClientDB{
 		Timestamp: 1528482271834,
-		Data:      []Addon{gatherer},
+		Addons:    []Addon{gatherer},
 	}
 	fixture, err := os.Open("./fixtures/clientdb/gatherer.json")
 	if err != nil {
