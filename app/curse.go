77138a618a8d92fe05dd71ad46955e51aa0a7431
Coline Carle <sweetlie@wipeinc.io>
2018-07-14 12:46:44 +0200

    improvement: remove unnecessary transaction for import

--- app/curse.go
+++ app/curse.go
@@ -3,7 +3,9 @@ package app
 import (
 	"compress/bzip2"
 	"database/sql"
+	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/coline-carle/zhevra/addon/metadata/curseforge"
 	"github.com/pkg/errors"
@@ -13,13 +15,17 @@ const url = "http://clientupdate-v6.cursecdn.com/feed/addons/1/v10/complete.json
 
 // DownloadDatabase download and decode a curseforge database
 func (a *App) DownloadDatabase() (*curseforge.ClientDB, error) {
+	start := time.Now()
 	resp, err := http.Get(url)
 	if err != nil {
 		errors.Wrap(err, "Download Database network error")
 	}
 	defer resp.Body.Close()
 	curseDB := bzip2.NewReader(resp.Body)
-	return curseforge.DecodeDB(curseDB)
+	clientdb, err := curseforge.DecodeDB(curseDB)
+	elapsed := time.Now().Sub(start)
+	fmt.Printf("downloaded database in %v\n", elapsed)
+	return clientdb, err
 }
 
 // ImportCurseDB import complete curse database
@@ -29,9 +35,7 @@ func (a *App) ImportCurseDB(curseDB *curseforge.ClientDB) error {
 	err = a.storage.Tx(func(tg *sql.Tx) error {
 		for _, addon := range curseDB.Addons {
 			curseAddon := curseforge.NewCurseAddon(addon)
-			err = a.storage.Tx(func(tx *sql.Tx) error {
-				return a.storage.CreateCurseAddon(tx, curseAddon)
-			})
+			err = a.storage.CreateCurseAddon(tg, curseAddon)
 			if err != nil {
 				return err
 			}
